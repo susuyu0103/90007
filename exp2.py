f030723c434b8f97024138cc046b2d99ac87d2d8
susuyu0103 <70868333+susuyu0103@users.noreply.github.com>
2020-09-21 10:30:23 +0800

    Add files via upload

---
+++ exp2.py
@@ -0,0 +1,49 @@
+# string = 'I love you'
+# print(string[::-1])
+
+# stri = 'I love you'
+# res = stri.split(' ')
+# print(res)
+
+# a="so "*100
+# print("I love you ",a,"much")
+
+import random as r
+a = r.randint(1,100)
+print(a)
+b="so"*a
+print("I love you ",b,"much")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
