eb80b460964e5c139ed54fafcee2e60076f6771e
susuyu0103 <70868333+susuyu0103@users.noreply.github.com>
2020-09-21 10:23:16 +0800

    Add files via upload

---
+++ exp2.py
@@ -0,0 +1,65 @@
+# string = 'I love you'
+# print(string[::-1])
+
+# stri = 'I love you'
+# res = stri.split(' ')
+# print(res)
+
+# sum=0
+# number=int(input('請輸入整數: '))
+  
+# #遞增for迴圈,從小到大打印出數字 
+# print('從小到大排列輸出數字:')
+# for i in range(1,number+1):
+#     sum+=i  #設定sum為i的和 
+#     print('%d' %i,end='')
+#     #設定輸出連加的算式 
+#     if i<number:
+#         print('+',end='')
+#     else:
+#         print('=',end='')
+# print('%d' %sum)
+
+
+# a="so "*100
+# print("I love you ",a,"much")
+
+import random as r
+a = r.randint(1,100)
+print(a)
+b="so"*a
+print("I love you ",b,"much")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
